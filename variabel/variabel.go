08e02bf03b3fe03713f84f58ba850cb5a96dfcc7
malikizaki94 <malikizakitop@gmail.com>
2023-10-14 23:08:32 +0700

    variabel

---
+++ variabel/variabel.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var namaVariabel string // Deklarasi variabel string
+	var umur int            // Deklarasi variabel integer
+
+	namaVariabel = "John" // Menetapkan nilai ke variabel namaVariabel
+	umur = 30             // Menetapkan nilai ke variabel umur
+
+	fmt.Println("Nama:", namaVariabel)
+	fmt.Println("Umur:", umur)
+}
